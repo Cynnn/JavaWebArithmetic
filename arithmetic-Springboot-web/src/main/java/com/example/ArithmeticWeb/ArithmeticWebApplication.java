8a3d2c64d63522fffae7f323fe74ce504456502a
CHY\dell <353663785@qq.com>
2017-10-06 20:30:18 +0800

    update calculate and time

--- arithmetic-Springboot-web/src/main/java/com/example/ArithmeticWeb/ArithmeticWebApplication.java
+++ arithmetic-Springboot-web/src/main/java/com/example/ArithmeticWeb/ArithmeticWebApplication.java
@@ -1,28 +1,28 @@
 package com.example.ArithmeticWeb;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-@Controller
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 @SpringBootApplication
-@EnableAutoConfiguration
 public class ArithmeticWebApplication {
-	@RequestMapping("/")
-    public String index(Model model) {
-        ArrayList<ArithmeticExpression> Expression = new ArrayList<>();
-        String s1 = ArithmeticExpression.random();
-        String s2 = ArithmeticExpression.random();
-        ArithmeticExpression e1 = new ArithmeticExpression(s1,ArithmeticController.evaluateAlgorithm(s1));
-        ArithmeticExpression e2 = new ArithmeticExpression(s2,ArithmeticController.evaluateAlgorithm(s2));
-        Expression.add(e1);
-        Expression.add(e2);
-        model.addAttribute("expression", Expression);
-        return "index";
+	
+	@Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+
+            System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println(beanName);
+            }
+
+        };
     }
 	public static void main(String[] args) {
 		SpringApplication.run(ArithmeticWebApplication.class, args);
