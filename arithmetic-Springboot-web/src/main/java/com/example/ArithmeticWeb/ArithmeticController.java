38b19231bd0ceeae34fc08abbfb475a0e5ddcfbf
CHY\dell <353663785@qq.com>
2017-10-07 22:15:15 +0800

    all function realize

--- arithmetic-Springboot-web/src/main/java/com/example/ArithmeticWeb/ArithmeticController.java
+++ arithmetic-Springboot-web/src/main/java/com/example/ArithmeticWeb/ArithmeticController.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import question.Question;
 
 @Controller
@@ -14,16 +17,59 @@ public class ArithmeticController {
 	@Autowired
 	private ArthmeticService arthmeticService;
 	
-	@RequestMapping(value = {"","/login"},method = RequestMethod.GET)
+	@RequestMapping(value = "/login",method = RequestMethod.GET)
     public String login(){  
         return "login";  
     }
 	
-	@RequestMapping("/")
-    public String index(Model model) {
-        ArrayList<Question> Expression = arthmeticService.getQuestionList(10);
+	@RequestMapping(value ="/number",method = RequestMethod.GET)
+    public String getNumber(Model model,@RequestParam(value="Action",required=true) String action,
+    		@RequestParam(value="Number",required=true)String number) {
+		int num = Integer.parseInt(number);
+        ArrayList<Question> Expression = arthmeticService.getQuestionList(num);
         model.addAttribute("expression", Expression);
+        int right=0;
+        int wrong=0;
+        String str =arthmeticService.readTxtFile();
+		if(str!=null&&!str.equals("")){
+		String[] splits = str.split("\t");
+		right = Integer.parseInt(splits[0]);
+		wrong = Integer.parseInt(splits[1]);
+		}
+		model.addAttribute("right", right);
+		model.addAttribute("wrong", wrong);
         return "index";
+    }
+	@RequestMapping(value ="/")
+    public String index(Model model) {
+		ArrayList<Question> Expression = arthmeticService.getQuestionList(10);
+        model.addAttribute("expression", Expression);
+        int right=0;
+        int wrong=0;
+        String str =arthmeticService.readTxtFile();
+		if(str!=null&&!str.equals("")){
+		String[] splits = str.split("\t");
+		right = Integer.parseInt(splits[0]);
+		wrong = Integer.parseInt(splits[1]);
+		}
+		model.addAttribute("right", right);
+		model.addAttribute("wrong", wrong);
+        return "index";
+    }
+	@RequestMapping(value ="/index",method = RequestMethod.POST)
+    public String chkform(@RequestParam(value="Action",required=true)String action,
+    		@RequestParam(value="Right",required=true)int right,
+    		@RequestParam(value="Wrong",required=true)int wrong) {
+		String str =arthmeticService.readTxtFile();
+		if(str!=null&&!str.equals("")){
+		String[] splits = str.split("\t");
+		int r = Integer.parseInt(splits[0]);
+		int w = Integer.parseInt(splits[1]);
+		right+=r;
+		wrong+=w;
+		}
+		arthmeticService.writeTxtFile(right, wrong);
+        return "login";
     }
 
 }
--- arithmetic-Springboot-web/src/main/java/com/example/ArithmeticWeb/ArthmeticService.java
+++ arithmetic-Springboot-web/src/main/java/com/example/ArithmeticWeb/ArthmeticService.java
@@ -1,6 +1,11 @@
 package com.example.ArithmeticWeb;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Random;
 
 import org.springframework.stereotype.Service;
 
@@ -10,11 +15,59 @@ public class ArthmeticService {
 	
 	public ArrayList<Question> getQuestionList(int size){
 		ArrayList<Question> Expression = new ArrayList<>();
-		int operators_num = 2;
+		Random random = new Random();
+		int operators_num = random.nextInt(3)+1;
         for(int i=0;i<size;i++){
         	Expression.add(new Question(operators_num));
         }
         return Expression;
 	}
+	
+	public boolean writeTxtFile(int right,int wrong){
+		File fileName = new File("/record.txt");
+		String content = right +"\t"+wrong;
+		try{
+			if(!fileName.exists()){  
+			    fileName.createNewFile();
+			}
+			FileOutputStream o=new FileOutputStream(fileName);
+			o.write(content.getBytes("GBK"));  
+		    o.close();  
+		    return true;
+		}catch(Exception e){
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	public String readTxtFile(){  
+		  String result=null;  
+		  FileReader fileReader=null;  
+		  BufferedReader bufferedReader=null;  
+		  File fileName = new File("/record.txt");
+		  System.out.println(fileName.getAbsolutePath());
+		  try{  
+			  if(!fileName.exists()){  
+				    fileName.createNewFile();
+				    return "";
+				}		   
+		   fileReader=new FileReader(fileName);  
+		   bufferedReader=new BufferedReader(fileReader);  
+		   String read=null;  
+		    while((read=bufferedReader.readLine())!=null){  
+		     result=read; 
+		    }
+		    if(bufferedReader!=null){  
+			    bufferedReader.close();  
+			   }  
+			   if(fileReader!=null){  
+			    fileReader.close();  
+			   }  
+		  }catch(Exception e){  
+		   e.printStackTrace();  
+		  } 
+		  System.out.println("读取出来的文件内容是："+"\r\n"+result);  
+		  return result;  
+		 }  
 
 }
